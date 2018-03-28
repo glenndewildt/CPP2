bdd78395880bd33e920743eff4fcd10f56bc9a1e
Mark van Kleef <mkleef@avans.nl>
2018-03-28 20:56:03 +0200

    Added Classes we might need.

---
+++ CPP2/CPP2/Stacks.cpp
@@ -0,0 +1,19 @@
+#include "Stacks.h"
+
+#include <string>
+#include <iterator>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+#include "Globals.h"
+
+using namespace std;
+
+Stacks::Stacks()
+{
+}
+
+Stacks::~Stacks()
+{
+}
+
---
+++ CPP2/CPP2/Stacks.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <deque>
+
+#include "BuildingCard.h"
+#include "CharacterCard.h"
+
+class Stacks
+{
+public:
+	Stacks();
+	~Stacks();
+
+	void initBuildingCards();
+	void initCharacterCards();
+	void shuffleBuildingCards();
+	void shuffleCharacterCards();
+
+	const BuildingCard getBuildingCard();
+
+	const std::string getCharacterCardOptions();
+
+	const int getAmountOfCharacterCards();
+
+	void discardBuildingCard(const BuildingCard b);
+
+	const CharacterCard getCharacterCard(const int optionId);
+
+	const std::string removeCharacterCard(const int optionId);
+
+	void addCharacterCard(const CharacterCard card);
+
+	void undiscardCharacterCards();
+
+private:
+	std::deque<BuildingCard> buildingCards;
+	std::deque<CharacterCard> characterCards;
+
+	std::deque<BuildingCard> discardedBuildings;
+	std::deque<CharacterCard> discardedCharacters;
+
+};
