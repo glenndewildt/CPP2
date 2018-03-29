4d4de0449bb9252294bc58896109aee02847d6b6
glenndewildt <glenndewildt@gmail.com>
2018-03-29 15:44:16 +0200

    er zitten wel memmory leaks in weet niet waarom

--- CPP2/CPP2/Player.h
+++ CPP2/CPP2/Player.h
@@ -10,6 +10,9 @@
 #define Player_hpp
 
 #include <string>
+#include<vector>
+#include "BuildingCard.h"
+#include "CharacterCard.h"
 
 class Player {
 public:
@@ -18,10 +21,15 @@ public:
     std::string get_name() const { return name; }
     void set_name(const std::string& new_name) { name = new_name; }
 
+
 	const int id;
 
 private:
     std::string name;
+	int gold;
+	std::vector<CharacterCard> character_carts;
+	std::vector<BuildingCard> building_carts;
+
 };
 
 #endif /* Player_hpp */
