e57df0d22e60a4651f8985d2b661f4365062e8bf
Mark van Kleef <mkleef@avans.nl>
2018-03-31 16:27:58 +0200

    VS, why u no auto save? part 2

--- CPP2/CPP2/Player.h
+++ CPP2/CPP2/Player.h
@@ -10,6 +10,10 @@
 #define Player_hpp
 
 #include <string>
+#include <vector>
+
+#include "BuildingCard.h"
+#include "CharacterCard.h"
 
 class Player {
 public:
@@ -18,10 +22,26 @@ public:
     std::string get_name() const { return name; }
     void set_name(const std::string& new_name) { name = new_name; }
 
+	int get_gold() const { return gold; }
+	void set_gold(const int& new_gold) { gold = new_gold; }
+
+
+	std::vector<CharacterCard> getCharCards() const { return charCards; }
+	void addCharCard(const CharacterCard& newCard) { charCards.push_back(newCard); }
+	//void delCharCard(const CharacterCard& new_char_cart) { character_carts.erase(new_char_cart); }
+
+	std::vector<BuildingCard> getBuildingCards() const { return buildingCards; }
+	void addBuildingCard(const BuildingCard& newCard) { buildingCards.push_back(newCard); }
+	//void delBuildingCard(const BuildingCard& newCard) { building_carts.erase(newCard); }
+
+
 	const int id;
 
 private:
-    std::string name;
+	int gold;
+	std::string name;
+	std::vector<CharacterCard> charCards;
+	std::vector<BuildingCard> buildingCards;
 };
 
 #endif /* Player_hpp */
