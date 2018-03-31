160eb289ee111aaa75b1e4a486c7c22d1ad6f58c
Mark van Kleef <mkleef@avans.nl>
2018-03-31 16:31:20 +0200

    Even more merge f*ck ups

--- CPP2/CPP2/CharacterCard.h
+++ CPP2/CPP2/CharacterCard.h
@@ -1,6 +1,34 @@
 #pragma once
 
+#include <iostream>
+using namespace std;
 class CharacterCard
 {
+public:
+	CharacterCard() :number{ "" }, kind{ "" } {};
+	CharacterCard(const string kind, const string cost) : number{ kind }, kind{ cost } {};
+	~CharacterCard() {};
+
+	/*    friend ostream& operator<<(ostream& strm, BuildingCard & cart) {
+	if (strm) {
+	strm << cart.kind;
+	}
+
+	return strm;
+	}
+	*/
+	const bool is_empty() const {
+
+		return number.empty() && kind.empty();
+	}
+
+
+	const string get_kind() const { return number; }
+	const string get_cost() const { return kind; }
+
+private:
+	const string number;
+	const string kind;
+
 
 };
